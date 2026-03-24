17e7dddcd6757ab8c2124af6a79a099b1148ea48
polina12 <polkriv@gmail.com>
2026-03-24 17:48:41 +0300

    task2

--- solutions/1-syntax.js
+++ solutions/1-syntax.js
@@ -1,3 +1,10 @@
 // BEGIN (write your solution here)
+export const getWeekends = (format = 'long') => {
+    const weekends = {
+        long: ['saturday', 'sunday'],
+        short: ['sat', 'sun']
+    };
 
+    return weekends[format];
+};
 // END
