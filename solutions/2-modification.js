d5569f5c2d5243168cc3573f8a3a1e7f0c52dcc5
polina12 <polkriv@gmail.com>
2026-03-25 13:40:07 +0300

    Add solutions

--- solutions/2-modification.js
+++ solutions/2-modification.js
@@ -1,3 +1,12 @@
 // BEGIN
+const swap = (arr) => {
+    if (arr.length <= 1) return [...arr];
 
+    const result = [...arr];
+    [rasult[0], result[result.lengh - 1]] = [result[result.length - 1], result[0]];
+
+    return result;
+};
+
+export default swap;
 // END
