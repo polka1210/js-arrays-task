0ecb05b466c762c06a82dec646debbaee862309d
polina12 <polkriv@gmail.com>
2026-03-25 14:03:25 +0300

    task3

--- solutions/3-isset.js
+++ solutions/3-isset.js
@@ -1,3 +1,10 @@
 // BEGIN
+const get = (array, index, defaultvalue = null) => {
+    if (index >= 0 && index < array.lengh) {
+        return array[index];
+    }
+    return defaultvalue;
+};
 
+export default get;
 // END
