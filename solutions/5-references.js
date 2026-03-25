44b687a4a9f4e8f9deda5fc5ccf3a0e920fdafb8
polina12 <polkriv@gmail.com>
2026-03-25 14:19:35 +0300

    task5-6

--- solutions/5-references.js
+++ solutions/5-references.js
@@ -1,3 +1,13 @@
 // BEGIN
+const reverse = (arr) => {
+    for (let x = 0; x < Math.floor(arr.length / 2); x++) {
+        const y = arr.length - 1 - x;
+        const temp = arr[x];
+        arr[x] = arr[y];
+        arr[y] = temp;
+    }
+    return arr;
+};
 
+export default reverse;
 // END
--- solutions/6-aggregation.js
+++ solutions/6-aggregation.js
@@ -1,3 +1,12 @@
 // BEGIN
-
+const calculateSum = (numbers) => {
+    let sum = 0;
+    for (let x = 0; x < numbers.length; x++) {
+       if (currentNumber > 10) {
+        sum += currentNumber; 
+    }
+}
+return sum;
+};
+export default calculateSum;
 // END
