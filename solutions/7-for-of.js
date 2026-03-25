4d032294c0619cf5daf8c7f6c1394481a36f6ad0
polina12 <polkriv@gmail.com>
2026-03-25 14:29:00 +0300

    tasks7-8

--- solutions/7-for-of.js
+++ solutions/7-for-of.js
@@ -1,3 +1,15 @@
 // BEGIN
+const calculateAverage = (temperatures) => {
+    if (temperatures.length === 0) {
+        return null;
+    }
+    let sum = 0;
+    for (const temp of temperatures) {
+        sum += temp;
+    }
+    const average = sum / temperatures.length;
+    return average;
+};
 
+export default calculateAverage;
 // END
--- solutions/8-removing.js
+++ solutions/8-removing.js
@@ -1,3 +1,19 @@
 // BEGIN
+const getSameParity = (numbers) => {
+    if (numbers.length === 0) {
+        return [];
+    }
+    const isFirstEven = Math.abs(numbers[0]) % 2 === 0;
+    const result = [];
+    for (let x = 0; x < numbers.length; x++) {
+        const currentNumber = numbers[x];
+        const isCurrenteven = Math.abs(currentNumber) % 2 === 0;
+        if (isCurrenteven === isFirstEven) {
+            result.push(currentNumber);
+        }
+    }
+    return result;
+};
 
+export default getSameParity;
 // END
