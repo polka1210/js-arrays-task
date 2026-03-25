e2f7bda87ebef529ba481372df62883eeb02bf91
polina12 <polkriv@gmail.com>
2026-03-25 14:12:41 +0300

    task4

--- solutions/4-for.js
+++ solutions/4-for.js
@@ -1,3 +1,11 @@
 // BEGIN
+const addPrefix = (items, prefix) => {
+    const result = [];
+    for (let i = 0; i < items.length; i++) {
+        result.push(`${prefix} ${items[i]}`);
+    }
+    return result;
+};
 
+export default addPrefix;
 // END
