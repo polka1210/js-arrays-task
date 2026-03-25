5ca598e29a060b6672841a8dbbc7f81776281588
polina12 <polkriv@gmail.com>
2026-03-25 14:43:51 +0300

    tasks9-10

--- solutions/10-nested-arrays.js
+++ solutions/10-nested-arrays.js
@@ -1,3 +1,25 @@
 // BEGIN
+const getSuperSeriesWinner = (scores) => {
+    let canadaScore = 0;
+    let ussrScore = 0;
+    for (let x = 0; x < scores.length; x++) {
+        const match = scores[x];
+        const canadaGoals = match[0];
+        const ussrGoals = match[1];
+        if (canadaGoals > ussrGoals) {
+            canadaScore++;
+        } else if (ussrGoals > canadaGoals) {
+            ussrScore++;
+        }
+    }
+    if (canadaScore > ussrScore) {
+        return 'canada';
+    } else if (ussrScore > canadaScore) {
+        return 'ussr';
+    } else {
+        return null;
+    }
+};
 
+export default getSuperSeriesWinner;
 // END
--- solutions/9-control-statements.js
+++ solutions/9-control-statements.js
@@ -1,3 +1,16 @@
 // BEGIN
+const getTotalAmount = (transactions, currency) => {
+    let total = 0;
+    for (let x = 0; x < transactions.length; x++) {
+        const transaction = transactions[x];
+        const [curr, amountStr] = transaction.split('');
+        const amount = Number(amountStr);
+        if (curr === currency) {
+            total += amount;
+        }
+    }
+    return total;
+};
 
+export default getTotalAmount;
 // END
